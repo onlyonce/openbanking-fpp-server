ebb831cca4fd0c1ab0194540debec85caa151c8a
Tamás Sipos <tamas.sipos@dpc.hu>
2020-01-22 00:57:24 +0100

    Update config/files/etc to docker-compose

--- src/main/resources/db/changelog/sql/init-1.0.sql
+++ src/main/resources/db/changelog/sql/init-1.0.sql
@@ -1,10 +1,10 @@
 INSERT INTO BANKS
-VALUES ('2293e2cf-1c54-38cf-9934-6be4e94e60f8', 'Lion', 'Lion', 'Lion Bank Ltd.', '/images/bank/lion.svg',
-        'https://api.lion.mlabs.dpc.hu:8243/token', 'https://api.lion.mlabs.dpc.hu:8243/open-banking/v3.1/aisp/v3.1.2',
-        'PttPN26uJLQgvRjSrhmh5ShaqZga', 'gUPRoq7QUgkuBkdLIkLc1d6fJhka', 'https://acefintech.mlabs.dpc.hu/lionfintech/customer/banks/authorize',
-        'acefintech', 'https://api.lion.mlabs.dpc.hu:8243/authorzize',
-        'https://api.lion.mlabs.dpc.hu:8243/open-banking/v3.1/pisp/v3.1.2',
-        'http://lion.mlabs.dpc.hu/accessschema/ob');
+VALUES ('2293e2cf-1c54-38cf-9934-6be4e94e60f8', 'Elephant', 'Elephant', 'Elephant Bank Ltd.', '/images/bank/elephant.svg',
+        'https://api.elephant.mlabs.dpc.hu:8243/token', 'https://api.elephant.mlabs.dpc.hu:8243/open-banking/v3.1/aisp/v3.1.2',
+        'PttPN26uJLQgvRjSrhmh5ShaqZga', 'gUPRoq7QUgkuBkdLIkLc1d6fJhka', 'https://api.elephant.mifos.io/customer/banks/authorize',
+        'acefintech', 'https://api.elephant.mlabs.dpc.hu:8243/authorzize',
+        'https://api.elephant.mlabs.dpc.hu:8243/open-banking/v3.1/pisp/v3.1.2',
+        'http://elephant.mlabs.dpc.hu/accessschema/ob');
 INSERT INTO USERS
 VALUES ('tppuser', '{bcrypt}$2a$10$FgRPdjDFcfxrCzWzVO/mZuWwVEm8CxqRNx4qQAOGVjzh/983lUPJy', TRUE);
 INSERT INTO AUTHORITIES
